aac28df6a41c4356c3447424a31c30e3c8af16fe
Bruno Anselmo <designer.brux@gmail.com>
2026-01-27 21:31:33 +0000

    correção import componentes

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,8 @@
-import Header from "../components/header";
 import Link from "next/link";
 
 export default function Contact() {
   return (
     <section className="container">
-      <Header />
-
       <main className="main-body">
         <div className="body-header">
           <Link href="/" className="back-btn">
@@ -15,24 +12,6 @@ export default function Contact() {
           <span className="large-p">Do you have a message for us?</span>
         </div>
       </main>
-
-      <footer className="footer">
-        <div className="footer-logo">
-          <Link href="/" className="logo-footer">
-            <h1 className="heading-primary">creative.index</h1>
-          </Link>
-          <p className="small-p">
-            All brand names and logos on creative.index are property of their
-            owners. This is an independent editorial project.
-          </p>
-        </div>
-
-        <nav className="nav-footer">
-          <Link href="/maison">by.maisons</Link>
-          <Link href="/creatives">by.creatives</Link>
-          <Link href="/contact">contact</Link>
-        </nav>
-      </footer>
     </section>
   );
 }
--- pages/maison.js
+++ pages/maison.js
@@ -1,11 +1,8 @@
-import Header from "../components/header";
 import Link from "next/link";
 
 export default function Maison() {
   return (
     <section className="container">
-      <Header />
-
       <main className="main-body">
         <div className="body-header">
           <Link href="/" className="back-btn">
@@ -18,24 +15,6 @@ export default function Maison() {
           </span>
         </div>
       </main>
-
-      <footer className="footer">
-        <div className="footer-logo">
-          <Link href="/" className="logo-footer">
-            <h1 className="heading-primary">creative.index</h1>
-          </Link>
-          <p className="small-p">
-            All brand names and logos on creative.index are property of their
-            owners. This is an independent editorial project.
-          </p>
-        </div>
-
-        <nav className="nav-footer">
-          <Link href="/maison">by.maisons</Link>
-          <Link href="/creatives">by.creatives</Link>
-          <Link href="/contact">contact</Link>
-        </nav>
-      </footer>
     </section>
   );
 }
