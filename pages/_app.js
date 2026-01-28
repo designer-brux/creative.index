f05b2405889ab6c598b0391926bece811d89418f
Bruno Anselmo <designer.brux@gmail.com>
2026-01-28 17:11:05 +0000

    adicionando google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "../styles/style.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
+  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
   return (
     <>
+      {gaId && <GoogleAnalytics gaId={gaId} />}
+      <GoogleAnalytics gaId="G-XTNX0FN03G" />
       <Header />
       <Component {...pageProps} />
       <Footer />
