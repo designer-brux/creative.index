ff971ad2e4ac6718645094de9500cdc044871786
Bruno Anselmo <designer.brux@gmail.com>
2026-01-28 20:43:49 +0000

    correção analytics

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,6 @@ export default function Header() {
   const toggleNav = () => setNavOpen(!navOpen);
 
   const handleNavClick = (category) => {
-    console.log("Evento disparado para:", category);
     setNavOpen(false);
     sendGAEvent({
       event: "nav_click", // Nome padronizado
--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,16 @@ import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
+  if (typeof window !== "undefined") {
+    window.gtag =
+      window.gtag ||
+      function () {
+        (window.dataLayer = window.dataLayer || []).push(arguments);
+      };
+    window.gtag("policy", "all", { essential: true, statistics: true });
+  }
+
   return (
     <>
       {gaId && <GoogleAnalytics gaId={gaId} />}
